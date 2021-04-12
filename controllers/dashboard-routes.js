4b07ecda577d4f8719b6b66e7c12b7e571092100
grey cashmere <dobsonemily@gmail.com>
2021-04-11 21:47:58 -0700

    fixing routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const sequelize = require('../config/connection.js');
 const { Post } = require("../models");
-const withAuth = require("../utils/auth");
+const withAuth = require("../utils/helpers");
 
 router.get("/", withAuth, async (req, res) => {
   try {
