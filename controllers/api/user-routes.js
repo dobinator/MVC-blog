1c09b6c08b6b56f6a0a1814672b2503e9d06d572
grey cashmere <dobsonemily@gmail.com>
2021-04-11 20:45:41 -0700

    link

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-const withAuth = require('../..utils/withAuth')
+const withAuth = require('../..utils/helpers');
 
 //expects email &password
 router.post('/login', async (req, res) => {
